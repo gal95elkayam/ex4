360d9199def0b6611f2693250797012a0ee605e6
gal <gal95elkayam@gmil.com>
2020-01-17 12:31:08 +0200

    finish

---
+++ cmake-build-debug/CMakeFiles/ex4.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ex4.dir/Matrix.cpp.o"
+  "CMakeFiles/ex4.dir/MyClientHandler.cpp.o"
+  "CMakeFiles/ex4.dir/MySerialServer.cpp.o"
+  "CMakeFiles/ex4.dir/MyTestClientHandler.cpp.o"
+  "CMakeFiles/ex4.dir/main.cpp.o"
+  "ex4"
+  "ex4.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ex4.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
